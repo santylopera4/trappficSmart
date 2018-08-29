d1324cf37511bd4652b5c4e6ece745f8b581491f
AlejoSamas <42448571+AlejoSamas@users.noreply.github.com>
2018-08-29 07:40:46 -0500

    actualizacion
    actualizacion del commit

---
+++ Strings/String.js
@@ -0,0 +1,36 @@
+var strings = {
+
+	equipo: [
+
+		{
+			nombre: ' Alejandro Gallego',
+			rol: 'desarrollador'
+		},
+		{
+			nombre: ' Santiago Lopera',
+			rol: 'desarrollador'
+		},
+		{
+			nombre: ' Laura ',
+			rol: 'desarrollador'
+		},
+		{
+			nombre: ' daniel',
+			rol: 'desarrollador'
+		},
+		{
+			nombre: ' jose',
+			rol: 'tutor'
+		},
+		{
+			nombre: ' cristian',
+			rol: 'tutor'
+		},
+		{
+			nombre: ' jaime',
+			rol: 'docente'
+		}
+
+
+	]
+}
