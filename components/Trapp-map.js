a2a992b583ff82d80bc9b301ab9fe27a395f0753
santylopera4 <41525908+santylopera4@users.noreply.github.com>
2018-08-30 11:49:03 -0500

    actualizacion
    actualizacion

--- components/Trapp-map.js
+++ components/Trapp-map.js
@@ -1,6 +1,12 @@
 Vue.component('trapp-map',{
     template:
-    `<div>  
+    `<div>
+    <div class="container">
+    <div class="row">
+    <div col-xs-12>  
 	 <img src="old/visualizar parqueaderos/img/fondomapa.png">
+	 </div>
+	 </div>
+	 </div>
     </div>`
 })
