7a433b923af79b65d50c04537960470c1328b580
AlejoSamas <42448571+AlejoSamas@users.noreply.github.com>
2018-09-03 07:45:47 -0500

    miembros del preoyecto
    se ubico correctamente la imagen de algunos miembros y se destaco su funcion en este proyecto

--- components/Trapp-member.js
+++ components/Trapp-member.js
@@ -1,12 +1,60 @@
 Vue.component('trapp-member',{
     template:
-    `<div class = "container">
-    	<div class = "row">	
-    		<div class = "col-xs"
-    		<h6> Jaime Echavarria</h6>
-    	</div>
+    `<div class = "container-fluid">
     	<div class = "row">
+    		<img class = miembros src="Assets/perfil.jpg"/>	
+    		<h6 class = "titulo"> Jaime Echavarria</h6>
+    	</div>
+    	<div class = "row tx">
     		<p> docente enlace del concurso nacional</p>
+    	</div>
+    	<br>
+    	<div class = "row">
+    		<img class = miembros src="Assets/jose.jpg"/>	
+    		<h6 class = "titulo">Jose</h6>
+    	</div>
+    	<div class = "row tx">
+    		<p>Instructor del concurso nacional</p>
+    	</div>
+    	<br>
+    	<div class = "row">
+    		<img class = miembros src="Assets/cristian.jpg"/>	
+    		<h6 class = "titulo">cristian</h6>
+    	</div>
+    	<div class = "row tx">
+    		<p>Instructor del concurso nacional</p>
+    	</div>
+    	<br>
+    	<div class = "row">
+    		<img class = miembros src="Assets/perfil.jpg"/>	
+    		<h6 class = "titulo">Daniel lopera</h6>
+    	</div>
+    	<div class = "row tx">
+    		<p>estudiante del concurso nacional</p>
+    	</div>
+    	<br>
+    	<div class = "row">
+    		<img class = miembros src="Assets/perfil.jpg"/>	
+    		<h6 class = "titulo">Santiago lopera</h6>
+    	</div>
+    	<div class = "row tx">
+    		<p>estudiante del concurso nacional</p>
+    	</div>
+    	<br>
+    	<div class = "row">
+    		<img class = miembros src="Assets/perfil.jpg"/>	
+    		<h6 class = "titulo">laura Escobar</h6>
+    	</div>
+    	<div class = "row tx">
+    		<p>estudiante del concurso nacional</p>
+    	</div>
+    	<br>
+    	<div class = "row">
+    		<img class = miembros src="Assets/Alejandro.jpeg"/>	
+    		<h6 class = "titulo">Alejandro Gallego</h6>
+    	</div>
+    	<div class = "row tx">
+    		<p>estudiante del concurso nacional</p>
     	</div>
     </div>`
 })
