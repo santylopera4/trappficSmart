9016ae1a9b7c1acc883186bf9900d6098946c37c
Daniel Lopera Gomez <peke5237@gmail.com>
2018-09-06 09:47:58 -0500

    trapp-nav-mobile
    se intenta actualizar el sidebar

--- components/Trapp-li-nav.js
+++ components/Trapp-li-nav.js
@@ -1,10 +1,40 @@
-Vue.component('trapp-li-nav',{
-    props:['icono','texto'],
+Vue.component('trapp-li-nav', {
+    props: ['icono', 'texto'],
     template:
-    `<div class="container opcion">
+        `<div class="container opcion">
+    <a class="nav-link li-nav" href="#" >
     <i class="fas fa-home"></i>
-       <p class="text1">
-           inicio
-       </p>
+    <p class="text1">
+        inicio
+    </p>
+    </a>
+    <a class="nav-link li-nav" href="#">
+    <i class="fas fa-user-circle"></i>
+    <p class="text1">
+        Perfil
+    </p>
+    </a>
+    <a class="nav-link li-nav" href="#">
+    <i class="fas fa-parking"></i>
+    <p class="text1">
+        Parqueadero
+    </p>
+    </a>
+         <a class="nav-link li-nav" href="#">
+         <i class="fas fa-question-circle"></i>
+         <p class="text1">
+             Ayuda
+         </p>
+         </a>
+    
+    
+         <a class="nav-link li-nav" href="#">
+          <i class="fas fa-question-circle"></i>
+          <p class="text1">
+              Cerrar Sesion
+          </p>
+          </a>
+
+         
  </div>`
 })
--- components/Trapp-nav-mobile.js
+++ components/Trapp-nav-mobile.js
@@ -1,20 +1,50 @@
 Vue.component('trapp-nav-mobile', {
   template:
-    `<div class="nav1 container">
-    <nav class="navbar navbar-light" style="background-color: #fad800;">
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#desplieguenavbar" aria-controls="desplieguenavbar" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <a class="navbar-brand" href="#">TrappFic Smart</a>
-  <div class="collapse navbar-collapse" id="desplieguenavbar">
-    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-    <li>
-    <a class="nav-link" href="#">
-    <trapp-li-nav></trapp-li-nav>
-    </a>
-    </li>
-    </ul>
-  </div>
-</nav> 
+    `<div class=" container">
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark sidebarNavigation" data-sidebarClass="navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler leftNavbarToggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault"
+            aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home
+                        <span class="sr-only">(current)</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="#">Disabled</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="https://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true"
+                        aria-expanded="false">Dropdown</a>
+                    <div class="dropdown-menu" aria-labelledby="dropdown01">
+                        <a class="dropdown-item" href="#">Action</a>
+                        <a class="dropdown-item" href="#">Another action</a>
+                        <a class="dropdown-item" href="#">Something else here</a>
+                    </div>
+                </li>
+            </ul>
+    
+        <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
+        </div>
+    </nav>
+
+    <script>
+    window.onload = function () {
+    if (window.jQuery) {
+        $(document).ready(function () { $(".sidebarNavigation .navbar-collapse").hide().clone().appendTo("body").removeAttr("class").addClass("sideMenu").show(); $("body").append("<div class='overlay'></div>"); $(".navbar-toggle, .navbar-toggler").on("click", function () { $(".sideMenu").addClass($(".sidebarNavigation").attr("data-sidebarClass")); $(".sideMenu, .overlay").toggleClass("open"); $(".overlay").on("click", function () { $(this).removeClass("open"); $(".sideMenu").removeClass("open") }) }); $("body").on("click", ".sideMenu.open .nav-item", function () { if (!$(this).hasClass("dropdown")) { $(".sideMenu, .overlay").toggleClass("open") } }); $(window).resize(function () { if ($(".navbar-toggler").is(":hidden")) { $(".sideMenu, .overlay").hide() } else { $(".sideMenu, .overlay").show() } }) })
+    } else { console.log("sidebarNavigation Requires jQuery") }
+}
+    </script>
     </div>`
 })
