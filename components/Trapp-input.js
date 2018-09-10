61cc2756409e4c51f0f95bf8080d38c703dc501a
santylopera4 <41525908+santylopera4@users.noreply.github.com>
2018-09-10 00:21:07 -0500

    icono
    icono

--- components/Trapp-input.js
+++ components/Trapp-input.js
@@ -1,10 +1,13 @@
 Vue.component('trapp-input',{
     template:
-    `<div>
+    `<div class="container">
+    <div class="row">
     <form>
     <label for="Nombre">Nombre</label>
     <br>
+    <i class="fas fa-user"></i>
 	<input type="text" placeholder="Ingresa tu Nombre" maxlength="50" name="Nombre">
 	</form>
+	</div>
     </div>`
 })
