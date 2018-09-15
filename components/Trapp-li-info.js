f24ea269c34bbbe254702212bf01f19ef730c4c2
Lau-escobar28 <41348820+Lau-escobar28@users.noreply.github.com>
2018-09-15 15:53:29 -0500

    componente informacion
    trapp-li-info

--- components/Trapp-li-info.js
+++ components/Trapp-li-info.js
@@ -2,17 +2,13 @@ Vue.component('trapp-li-info',{
 	props: ['nombre' , 'telefono' , 'correo'],
     template:
     `<div>
-          <!--i class="nombre"></i>
-          <p> nombre: <strong> {{nombre}} </strong> </p>-->
-
           <i class="fas fa-user"></i>
-          <p> Nombre: <strong> {{nombre}} </strong> </p>
-
+          <label> <strong> Nombre: </strong>{{nombre}} </label>
+          <br>
           <i class="fas fa-phone"></i>
-          <p> Telefono: <strong> {{telefono}} </strong> </p>
-
+          <label> <strong> Telefono: </strong> {{telefono}} </label>
+          <br>
           <i class="fas fa-at"></i>
-          <p> Correo: <strong> {{correo}} </strong> </p>
-         
+          <label> <strong> Correo: </strong> {{correo}} </label>
     </div>`
 })
