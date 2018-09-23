9c9fe9a53ae80e6eafb7fe832e8a46a562b4f7dc
Lau-escobar28 <41348820+Lau-escobar28@users.noreply.github.com>
2018-09-22 20:16:06 -0500

    pagina
    log in
    modificacion

--- pages/login.js
+++ pages/login.js
@@ -1,10 +1,16 @@
 const Login = {
     template: 
     `<div class="loginBox">
-        <img src="Assets.perfil.jpg" class="user">  
-        <h2>Log In</h2>
-        <trapp-input :item="Strings.trapp_input['Usuario']"></trapp-input>
-        <trapp-input :item="Strings.trapp_input['Contraseña']"></trapp-input>
+        <img src="Assets/perfil.jpg" class="user">  
+        <h2>Iniciar Sesión</h2>
+                    <!-- trapp-input :item="Strings.trapp_input['Usuario']"></trapp-input>
+                    <trapp-input :item="Strings.trapp_input['Contraseña']"></trapp-input-->
+        <form>
+        <p>Usuario</p>
+        <input type="text" name="" placeholder="Ingrese su nombre de usuario">
+        <p>Contraseña</p>
+        <input type="password" name="" placeholder="Ingrese su contraseña">
+        </form>
         <input type="submit" name="" value="Ingreasar">
         <a href="#">Registrarme</a>
         <a href="#">Olvidé mi contraseña</a>
