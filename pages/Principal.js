acaec6b3965272027e04fae8209fe2ce0efb832f
santylopera4 <41525908+santylopera4@users.noreply.github.com>
2018-09-26 23:37:54 -0500

    Principal
    Principal

--- pages/Principal.js
+++ pages/Principal.js
@@ -1,3 +1,14 @@
 const Principal = {
-    template: `<div></div>`
+    template: `<div><div class="encabezado">
+    <div class="container">
+    <div class="row justify-content(center) d-flex-center  ">
+        <div class="col1 col align-self-center col-6">
+           <a href="#"><h1>{{texto}}</h1></a>
+        </div>
+        <div class="col2 col align-self-auto col-6">
+        <a href="#"><img :src="icono" ></a>        
+        </div>
+    </div>
+</div>
+    </div></div>`
 }
