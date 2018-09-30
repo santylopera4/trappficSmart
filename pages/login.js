823583ae5a3de21599f792ccfd5795040fa6aa72
Lau-escobar28 <41348820+Lau-escobar28@users.noreply.github.com>
2018-09-29 19:26:59 -0500

    pages
    login

--- pages/login.js
+++ pages/login.js
@@ -6,13 +6,15 @@ const Login = {
                     <!-- trapp-input :item="Strings.trapp_input['Usuario']"></trapp-input>
                     <trapp-input :item="Strings.trapp_input['Contraseña']"></trapp-input-->
         <form>
-        <p>Usuario</p>
+        <i class="fas fa-user"></i>
+        <label>Usuario</label>
         <input type="text" name="" placeholder="Ingrese su nombre de usuario">
-        <p>Contraseña</p>
+        <i class="fas fa-unlock"></i>
+        <label>Contraseña</label>
         <input type="password" name="" placeholder="Ingrese su contraseña">
         </form>
         <input type="submit" name="" value="Ingreasar">
-        <a href="#">Registrarme</a>
+        <a href="#">Registrarme</a> 
         <a href="#">Olvidé mi contraseña</a>
     </div>`
 }
