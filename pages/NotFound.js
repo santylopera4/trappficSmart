2502db5f9cd83349bb5ab769ba68fe370d665339
Daniel Lopera Gomez <peke5237@gmail.com>
2018-10-01 16:12:11 -0500

    paginas de notfound  e info parqueadero
    aca se entrega las paginas mencionadas lo mejor hechas posible

--- pages/NotFound.js
+++ pages/NotFound.js
@@ -1,22 +1,21 @@
 Vue.component('not-found',{
     props:[''],
     template:
-    `<div>
-    <input type="checkbox" id="cerrar" class="salir">
-    <label for="cerrar" id="btn-cerrar">
-            <div class="col col-12 justify-content-center">
-                <div class="alert alert-dark" role="alert">            
-            
-            <button type="button" class="close boton1" data-dismiss="alert" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-            </button> 
-                    <h4 class="alert-heading titulo">Lo sentimos!</h4>
-                    <br>
-                    <p class="texto1">En este momento la pagina no esta funcionando correctamente o tiene un error, por favor intentalo mas tarde o vuelve a recargar la pagina.</p>
-                    <hr>
-                    <p class="mb-0 texto2">Gracias por su atencion.</p>
-                </div>
-            </div>
-    </label>
+	`<div class="notfound">
+	<header>
+		<div class="titulo">
+			<h1 class="title">Lo sentimos!!<img src="Assets/iconnotfound.png" alt=""></h1>
+			<br>
+			<div class="texto">
+					<p>
+						<span>
+
+						Marlon es un crack 
+
+						</span>
+				</p>
+			</div> 
+		</div>
+	</header>
     </div>`
-})
+}) 
