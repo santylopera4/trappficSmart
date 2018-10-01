a64f1e30cd45d76aa49b6cba16d36c27fbd8c1fe
Daniel Lopera Gomez <peke5237@gmail.com>
2018-10-01 18:26:21 -0500

    notfooun e infop
    aca se actuliza un poco la interfaz grafica

--- pages/InfoParqueadero.js
+++ pages/InfoParqueadero.js
@@ -4,50 +4,65 @@ Vue.component('info-parqueadero',{
     `<div>
     <div class="container-fluid infop">
 
-    <div class="Nombre">
-    <h1>Nombre del Praqueadero</h1>
+    <div>
+    <h1 class="Nombre">Nombre del Praqueadero</h1>
     </div>
     <hr>
 
-    <div id="carouselExampleIndicators" class="images carousel slide" data-ride="carousel">
-    <ol class="carousel-indicators">
-      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-    </ol>
-    <div class="carousel-inner">
-      <div class="carousel-item active">
-        <img class="imagenes d-block w-100" src="Assets/image1slidebar.jpg" alt="First slide">
-      </div>
-      <div class="carousel-item">
-        <img class="imagenes d-block w-100" src="Assets/image2slidebar.jpg" alt="Second slide">
-      </div>
-      <div class="carousel-item">
-        <img class="imagenes d-block w-100" src="Assets/image3slidebar.jpg" alt="Third slide">
-      </div>
-    </div>
-    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="sr-only">Previous</span>
-    </a>
-    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="sr-only">Next</span>
-    </a>
-  </div>
   
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="imagenes d-block w-100" src="Assets/image1slidebar.jpg" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="imagenes d-block w-100" src="Assets/image2slidebar.jpg" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="imagenes d-block w-100" src="Assets/image3slidebar.jpg" alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+<br>
+<br>
+
     <div class="infomacion">
-        <div class="par"><h5>Telefono:</h5>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, quod, fugit consectetur nobis, fugiat modi voluptatum debitis ipsum dolorem dolorum veniam. Reprehenderit nostrum enim a ad natus sed unde at.</p><hr></div>
-        <div class="par"><h5>Ubicacion:</h5>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, quod, fugit consectetur nobis, fugiat modi voluptatum debitis ipsum dolorem dolorum veniam. Reprehenderit nostrum enim a ad natus sed unde at.</p><hr></div>
-        <div class="par">        <h5>Cupos:</h5>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, quod, fugit consectetur nobis, fugiat modi voluptatum debitis ipsum dolorem dolorum veniam. Reprehenderit nostrum enim a ad natus sed unde at.</p><hr></div>
-        <div class="par">        <h5>Tarifas:</h5>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, quod, fugit consectetur nobis, fugiat modi voluptatum debitis ipsum dolorem dolorum veniam. Reprehenderit nostrum enim a ad natus sed unde at.</p><hr></div>
-        <div class="par">        <h5>Extras:</h5>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, quod, fugit consectetur nobis, fugiat modi voluptatum debitis ipsum dolorem dolorum veniam. Reprehenderit nostrum enim a ad natus sed unde at.</p><hr></div>
-         <div class="boton">
+        <div class="par">
+        <h5>Telefono:</h5>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, quod, fugit consectetur nobis, fugiat modi voluptatum debitis ipsum dolorem dolorum veniam. Reprehenderit nostrum enim a ad natus sed unde at.</p>
+        <hr></div>
+        <div class="par">        
+        <h5>Ubicacion:</h5>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, quod, fugit consectetur nobis, fugiat modi voluptatum debitis ipsum dolorem dolorum veniam. Reprehenderit nostrum enim a ad natus sed unde at.</p>
+        <hr></div>
+        <div class="par">        
+        <h5>Cupos:</h5>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, quod, fugit consectetur nobis, fugiat modi voluptatum debitis ipsum dolorem dolorum veniam. Reprehenderit nostrum enim a ad natus sed unde at.</p>
+        <hr></div>
+        <div class="par">        
+        <h5>Tarifas:</h5>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, quod, fugit consectetur nobis, fugiat modi voluptatum debitis ipsum dolorem dolorum veniam. Reprehenderit nostrum enim a ad natus sed unde at.</p>
+        <hr></div>
+        <div class="par">        
+        <h5>Extras:</h5>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, quod, fugit consectetur nobis, fugiat modi voluptatum debitis ipsum dolorem dolorum veniam. Reprehenderit nostrum enim a ad natus sed unde at.</p>
+        <hr></div>
+        
+        <div class="boton">
         <button class="reservar">Reservar</button>
         </div>
         <br>
