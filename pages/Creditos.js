cd512fafc2b53411ccc900fbe02f70c3049652fb
AlejoSamas <42448571+AlejoSamas@users.noreply.github.com>
2018-10-01 19:17:15 -0500

    Actualizacion
    Actualizacion de la pagina de creditos y recuoerar contraseña

--- pages/Creditos.js
+++ pages/Creditos.js
@@ -1,7 +1,115 @@
-Vue.component('cre-ditos',{
-    props:['item'],
+Vue.component('cre-ditos', {
+    props: ['item'],
     template:
-    `<div>
-    Pagina Creditos
+        `<div class = "container">
+            <h1>Creditos</h1>
+            <p
+            <p>
+            Esta página web está desarrollada por un excelente grupo, conformado por los instructores Cristian Marín 
+            y José Arango, que pudieron enseñar a los estudiantes a comprender el lenguaje de programación HTML, css y java script, los estudiantes 
+            Daniel Lopera, Laura Escobar, Santiago Lopera y Alejandro Gallego son el grupo de
+            programadores que le dieron cuerpo y estilo a la página web, y otro de los integrantes importantes de este grupo es el profesor Jaime Echavarría,
+            su papel fue guiar a los estudiantes para el desarrollo de las páginas web,además de que él es el Docente enlace que tiene el grupo de 
+            estudiantes.
+            </p>
+            <br>
+            <h1>Miembros</h1>
+            <div class = "row">
+                <div class = "col-xs-12 col-sm-2 col-md-2 col-lg-1">
+                    <img class = miembros src="Assets/jose.jpg"/> 
+                </div>
+                <div class = "col-xs-12 col-sm-10 col-md-10 col-lg-11">
+                    <div class = "row">
+                        <h6 class = "titulo">. <strong>Jose Arango</strong></h6>
+                    </div>
+                    <div class = "row">
+                    <p>. Instructor del concurso nacional</p>
+                    </div>
+                </div>
+            </div>
+            <br>
+            <div class = "row">
+                <div class = "col-xs-12 col-sm-2 col-md-2 col-lg-1">
+                    <img class = miembros src="Assets/cristian.jpg"/> 
+                </div>
+                <div class = "col-xs-12 col-sm-10 col-md-10 col-lg-11">
+                    <div class = "row">
+                        <h6 class = "titulo">. <strong>Cristian Marin</strong></h6>
+                    </div>
+                    <div class = "row">
+                    <p>. Instructor del concurso nacional</p>
+                    </div>
+                </div>
+            </div>
+            <br>
+            <div class = "row">
+                <div class = "col-xs-12 col-sm-2 col-md-2 col-lg-1">
+                    <img class = miembros src="Assets/perfil.jpg"/> 
+                </div>
+                <div class = "col-xs-12 col-sm-10 col-md-10 col-lg-11">
+                    <div class = "row">
+                        <h6 class = "titulo">. <strong>Daniel lopera</strong></h6>
+                    </div>
+                    <div class = "row">
+                    <p>. Estudiante del concurso nacional</p>
+                    </div>
+                </div>
+            </div>
+            <br>
+            <div class = "row">
+                <div class = "col-xs-12 col-sm-2 col-md-2 col-lg-1">
+                    <img class = miembros src="Assets/perfil.jpg"/> 
+                </div>
+                <div class = "col-xs-12 col-sm-10 col-md-10 col-lg-11">
+                    <div class = "row">
+                        <h6 class = "titulo">. <strong>Santiago lopera</strong></h6>
+                    </div>
+                    <div class = "row">
+                    <p>. Estudiante del concurso nacional</p>
+                    </div>
+                </div>
+            </div>
+            <br>
+            <div class = "row">
+                <div class = "col-xs-12 col-sm-2 col-md-2 col-lg-1">
+                    <img class = miembros src="Assets/perfil.jpg"/> 
+                </div>
+                <div class = "col-xs-12 col-sm-10 col-md-10 col-lg-11">
+                    <div class = "row">
+                        <h6 class = "titulo">. <strong>laura Escobar</strong></h6>
+                    </div>
+                    <div class = "row">
+                    <p>. Estudiante del concurso nacional</p>
+                    </div>
+                </div>
+            </div>
+            <br>
+            <div class = "row">
+                <div class = "col-xs-12 col-sm-2 col-md-2 col-lg-1">
+                    <img class = miembros src="Assets/Alejandro.jpg"/> 
+                </div>
+                <div class = "col-xs-12 col-sm-10 col-md-10 col-lg-11">
+                    <div class = "row">
+                        <h6 class = "titulo">. <strong>Alejandro Gallego</strong></h6>
+                    </div>
+                    <div class = "row">
+                        <p>. Estudiante del concurso nacional</p>
+                    </div>
+                </div>
+            </div
+            <br><br>
+            <div class = "row">
+            <div class = "col-xs-12 col-sm-2 col-md-2 col-lg-1">
+                <img class = miembros src="Assets/perfil.jpg"/> 
+            </div>
+            <div class = "col-xs-12 col-sm-10 col-md-10 col-lg-11">
+                <div class = "row">
+                    <h6 class = "titulo">. <strong>Jaime Echavarria</strong></h6>
+                </div>
+                <div class = "row">
+                    <p>. Docente enlace del concurso nacional</p>
+                </div>
+            </div>
+        </div>
     </div>`
 })
--- pages/RecuperarContraseña.js
+++ pages/RecuperarContraseña.js
@@ -1,7 +1,18 @@
 Vue.component('recuperar-contrasena',{
     props:[''],
     template:
-    `<div>
-       Pagina RecuperarContraseña
+    `<div class="container">
+       <h1>Recuperar Contraseña</h1><br><br>
+       <p>
+            Para recuperar tu contraseña coloca tu correo y envia el código de identificación a tu cuenta, despues ponlo en 
+            la parte de ingresar código.
+       </p>
+       <label for="correo">Ingresa Tu Correo</label>
+       <input type="email" placeholder="ingrese correo" id="correo">
+       <button><strong>Enviar</strong></button><br><br>
+       
+       <label for="correo">Ingrese el código</label>
+       <input type="Texto" placeholder="ingrese código" id="código">
+       <button><strong>Ingresar</strong></button><br><br>
     </div>`
 }) 
