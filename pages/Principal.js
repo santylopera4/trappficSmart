e60273fb7b3e4a54724bdd29e92e05ce6f957965
santylopera4 <santylprpt@gmail.com>
2018-10-03 06:59:10 -0700

    push
    push

--- pages/Principal.js
+++ pages/Principal.js
@@ -1,7 +1,8 @@
-Vue.component('prin-cipal',{
-    props:['trapp_li_nav'],
-    template:
+Vue.component('prin-cipal', {
+  props: ['trapp_li_nav', 'trapp_header'],
+  template:
     `<div>
+    <trapp-header-pc :trapp_header="trapp_header"></trapp-header-pc>
   <trapp-nav-mobile :trapp_li_nav="trapp_li_nav"><trapp-nav-mobile>
   <br>
  
