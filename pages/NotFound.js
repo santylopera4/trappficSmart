830cf7e6120ca3633767f7c5683f96eae65b1ec3
Daniel Lopera Gómez <DanielLoperaGomez@users.noreply.github.com>
2018-10-09 11:48:52 -0500

    Acabamos
    Damos por terminada la pagina web

--- pages/NotFound.js
+++ pages/NotFound.js
@@ -4,7 +4,7 @@ Vue.component('not-found',{
 	`<div class="container-fluid notfound">
 	<header>
 		<div class="titulo">
-			<h1 class="title">Lo sentimos!!<img src="Assets/iconnotfound.png" alt=""></h1>
+			<h1 class="title"><img src="Assets/not-found0.png" alt=""></h1>
 			<br>
 			<div class="texto">
 					<p>
