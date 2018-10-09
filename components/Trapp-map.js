05ad2a386a0e2c81421f352bb383cb89077813c9
santylopera4 <41525908+santylopera4@users.noreply.github.com>
2018-10-09 01:19:19 -0500

    Listo mapa
    y pagina principal

--- components/Trapp-map.js
+++ components/Trapp-map.js
@@ -1,4 +1,4 @@
-var mymap = L.map('mapid').setView([6.46026302, -75.55745681], 15);
+var mymap = L.map('mapid').setView([6.46026302, -75.55745681], 17);
 var OpenStreetMap_Mapnik = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
@@ -19,3 +19,13 @@ L.marker([6.462987, -75.558307], {draggable: true}).addTo(mymap)
 .bindPopup('Parqueadero Full Autos Belén');
 L.marker([6.462486, -75.554503], {draggable: true}).addTo(mymap)
 .bindPopup('Parqueadero La Capilla');
+L.marker([6.459618, -75.560109], {draggable: true}).addTo(mymap)
+.bindPopup('Parqueadero Barandales');
+L.marker([6.459703,  -75.559063], {draggable: true}).addTo(mymap)
+.bindPopup('Parqueadero El Trebol');
+L.marker([6.457687, -75.557655], {draggable: true}).addTo(mymap)
+.bindPopup('Parqueadero La Plazuela');
+L.marker([6.458718, -75.559703], {draggable: true}).addTo(mymap)
+.bindPopup('Parqueadero La Terminal');
+L.marker([6.45891, -75.556737], {draggable: true}).addTo(mymap)
+.bindPopup('Parqueadero San Pedro');
--- pages/Principal.js
+++ pages/Principal.js
@@ -1,13 +1,12 @@
 Vue.component('prin-cipal', {
-  props: ['trapp_li_nav', 'trapp_header'],
+  props: ['trapp_li_nav', 'trapp_header', 'icono', 'texto'],
   template:
     `<div> 
-  <trapp-nav-mobile :trapp_li_nav="trapp_li_nav"></trapp-nav-mobile>
-  <br>
-<<<<<<< HEAD
-  <div id="mapid"></div>
+          
+      <trapp-header-pc :icono="trapp_header.icono" :texto="trapp_li_nav[0].texto"></trapp-header-pc> 
+      <trapp-nav-mobile :trapp_li_nav="trapp_li_nav" ></trapp-nav-mobile>
+      <br>
+      <div id="mapid"></div>
+
     </div>`
-=======
-    </div>` 
->>>>>>> ffc7e6c7f090e802a02c8af1197ba076611ef53d
 }) 
