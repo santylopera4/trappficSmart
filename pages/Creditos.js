3ab9316817481ab048f88cd4acb5ba3061fcc26d
AlejoSamas <42448571+AlejoSamas@users.noreply.github.com>
2018-10-09 09:37:23 -0500

    producto final
    se va a mostrar el proceso ya finalizado del producto  viable de trappfic Smart

--- pages/Creditos.js
+++ pages/Creditos.js
@@ -1,11 +1,16 @@
 Vue.component('cre-ditos', {
-    props: ['array'],
+    props: ['array', 'trapp_li_nav','trapp_header', 'icono', 'texto'],
     template:
-        `<div class = "container1"><br>
+        `<div>
+        <div>
+            <trapp-header-pc :icono="trapp_header.icono" :texto="trapp_li_nav[2].texto"></trapp-header-pc> 
+            <trapp-nav-mobile :trapp_li_nav="trapp_li_nav" ></trapp-nav-mobile>
+        </div>
+        <div class = "container1"><br>
             <h1 class="credi">Creditos</h1>
-            <p class="rol">
+            <p class="rol1">
             Esta página web está desarrollada por un excelente grupo, conformado por los instructores Cristian Marín 
-            y José Arango, que pudieron enseñar a los estudiantes a comprender el lenguaje de programación HTML, css y javascript, los estudiantes 
+            y Jose Arango, que pudieron enseñar a los estudiantes a comprender el lenguaje de programación HTML, css y javascript, los estudiantes 
             Daniel Lopera, Laura Escobar, Santiago Lopera y Alejandro Gallego son el grupo de
             programadores que le dieron cuerpo y estilo a la página web, y otro de los integrantes importantes de este grupo es el profesor Jaime Echavarría,
             su papel fue guiar a los estudiantes para el desarrollo de las páginas web,además de que él es el Docente enlace que tiene el grupo de 
@@ -15,5 +20,6 @@ Vue.component('cre-ditos', {
             <h1 class="credi">Miembros:</h1><br>
             <trapp-member v-for="item in array" :icono="item.icono" :miembro="item.miembro" :rol="item.rol"></trapp-member>
             <br><br><br><br><br><br><br><br>
+            </div>
             </div>` 
 })
