e8cbdcb1e2a3d5316c6a0d3890ef217048d924c5
Daniel Lopera Gómez <DanielLoperaGomez@users.noreply.github.com>
2018-11-17 10:17:46 -0500

    login y registro
    se termino temporalment la actualixacion del login y el registro para asi manejar una base de datos en el aplucativo web

--- pages/login.js
+++ pages/login.js
@@ -1,22 +1,59 @@
-Vue.component('log-in',{
-    props:['item'],
+Vue.component('log-in', {
+    props: ['item'],
     template:
-    `<div class="d-flex login justify-content-center">
+        `<div class="d-flex login justify-content-center">
         <div class="loginBox align-self-center">
             <img src="Assets/perfil.jpg" class="user">  
             <h2>Iniciar Sesión</h2>
 
         <i class="fas fa-user"></i>
         <label for="email">Usuario</label>
-        <input type="text" name="email" id="txtEmail" placeholder="Ingrese su nombre de usuario">
+        <input type="text"  name="email" id="txtEmail" placeholder="Ingrese su nombre de usuario">
         <i class="fas fa-unlock"></i>
         <label for="password">Contraseña</label>
         <input type="password"  name="password" id="txtpassword" placeholder="Ingrese su contraseña">
-        <input type="submit" id="btnLogin"  name="ingrear" value="Ingresar">
-        <input type="submit" id="btnSiginUp" name="ingrear" value="registrarse">
-              
-        <a href="index.html?registro">Registrarme</a> 
-        <a href="index.html?recuperarcontrasena">Olvidé mi contraseña</a>
+        <input type="submit" id="btnLogin" v-on:click="loginpp" name="ingrear" value="Ingresar">
+        <input type="submit" id="btnSiginUp" v-on:click="registro" name="ingrear" value="registrarse">
+
         </div> 
-    </div>`
-})  
+    </div>`,
+    data: function () {
+        return {
+            loginpp: function () {
+                const txtEmail = document.getElementById('txtEmail');
+                const txtpassword = document.getElementById('txtpassword');
+                const btnLogin = document.getElementById('btnLogin');
+                const btnSiginUp = document.getElementById('btnSiginUp');
+
+
+
+                const email = txtEmail.value;
+                const pass = txtpassword.value;
+                const auth = firebase.auth();
+
+                const promise = auth.signInWithEmailAndPassword(email, pass);
+                promise.then(e => window.location.href="?principal");
+                
+                promise.catch(e => alert("verifica que el coreo y la contraseña esten bien escritas"));
+
+            },
+            registro: function () {
+
+                const txtEmail = document.getElementById('txtEmail');
+                const txtpassword = document.getElementById('txtpassword');
+                const btnLogin = document.getElementById('btnLogin');
+                const btnSiginUp = document.getElementById('btnSiginUp');
+
+
+                const email = txtEmail.value;
+                const pass = txtpassword.value;
+                const auth = firebase.auth();
+
+                const promise = auth.createUserWithEmailAndPassword(email, pass);
+                promise.then(e => alert("el correo se registro correctamente, por favor logeate para verificar tu correo "));
+                
+                promise.catch(e => alert("El usuario y la contraseña ya exixten"));
+
+            }
+        }
+    }})  
