644f8536418e59101b88ac23c4c6d91fb4176e53
santylopera4 <41525908+santylopera4@users.noreply.github.com>
2018-11-18 11:58:20 -0500

    cosito que le estorbaba a daniel

--- components/Trapp-map.js
+++ components/Trapp-map.js
@@ -1,8 +1,11 @@
-var mymap = L.map('mapid').setView([6.46026302, -75.55745681], 17);
-var OpenStreetMap_Mapnik = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+var mymap = L.map('mapid', { zoomControl:false }).setView([6.46026302, -75.55745681], 17);
+
+var OpenStreetMap_Mapnik = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', 
+{
     maxZoom: 19,
   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
       id: 'mapbox.streets',
+      
       accessToken: 'pk.eyJ1Ijoic2FudHl4Z21vIiwiYSI6ImNqbXpnbG11NjFvM3kzd3J1NzNpd3I4bHYifQ.ZGsBaXft_zntqyRsrbC2Aw'  
 }).addTo(mymap);
 L.marker([6.461419, -75.556896], {draggable: true}).addTo(mymap)
